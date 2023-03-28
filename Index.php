1a38edc53cbe19510ab967a28cd6f15648141244
Anthony <anthony.wetzstein@gmail.com>
2023-03-28 16:20:50 +0200

    Ajouter des pays

--- Index.php
+++ Index.php
@@ -3,6 +3,13 @@ spl_autoload_register(function ($class) {
     include $class . '.php';
 });
 $result ="";
+$france = new Pays("France");
+$allemagne = new Pays("Allemagne");
+$angleterre = new Pays("Angleterre");
+$italie = new Pays("Italie");
+$FCBarca = new Equipe("FC Barcelone", "1899", "Espagne");
+$PSG = new Equipe("Paris Saint-Germain", "1970", "France");
 $kylianmbapon = new Joueur("Mbappé", "Kylian", "20/12/1998", "Française");
 $lionelMessi = new Joueur("Messi", "Lionel", "24/06/1987", "Argentine");
-$PSG = new Equipe("Paris Saint-Germain", "1970", "France");
+
+echo $PSG->afficherJoueur();
