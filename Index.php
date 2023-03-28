1eb66179d83e4be68678fd371e04d84c0bce706a
Anthony <anthony.wetzstein@gmail.com>
2023-03-28 16:35:45 +0200

    Instancier RCS et un pays : espagne

--- Index.php
+++ Index.php
@@ -7,9 +7,12 @@ $france = new Pays("France");
 $allemagne = new Pays("Allemagne");
 $angleterre = new Pays("Angleterre");
 $italie = new Pays("Italie");
-$FCBarca = new Equipe("FC Barcelone", "1899", "Espagne");
-$PSG = new Equipe("Paris Saint-Germain", "1970", "France");
+$espagne = new Pays("Espagne");
+$FCBarca = new Equipe("FC Barcelone", "1899", $espagne);
+$PSG = new Equipe("Paris Saint-Germain", "1970", $france);
+$RCStbg = new Equipe("Racing Club Strasbourg", "1906", $france);
 $kylianmbapon = new Joueur("Mbappé", "Kylian", "20/12/1998", "Française");
 $lionelMessi = new Joueur("Messi", "Lionel", "24/06/1987", "Argentine");
 
+echo $france->afficherEquipes();
 echo $PSG->afficherJoueur();
