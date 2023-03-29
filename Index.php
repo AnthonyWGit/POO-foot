51444d54fe00ae000de55ce113fe716ca460f430
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-29 10:10:29 +0200

    ajouter des signatures

--- Index.php
+++ Index.php
@@ -14,7 +14,12 @@ $PSG = new Equipe("Paris Saint-Germain", "1970", $france);
 $RCStbg = new Equipe("Racing Club Strasbourg", "1906", $france);
 $kylianmbapon = new Joueur("Mbappé", "Kylian", "20/12/1998", "Française");
 $lionelMessi = new Joueur("Messi", "Lionel", "24/06/1987", "Argentine");
+$signatureMbappePSG = new Signature("2017",$PSG, $kylianmbapon);
+$signatureMessiBarca = new Signature("2004",$FCBarca,$lionelMessi);
+$ignatureMessiPSG = new Signature("2021", $PSG, $lionelMessi );
 
 echo $france->afficherEquipes();
 echo $espagne->afficherEquipes();
+//$PSG->ajouterJoueur($kylianmbapon);
 echo $PSG->afficherJoueur();
+
