af945b3cde646703e229048d7e3f680f8d70af3d
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-29 14:03:35 +0200

    affichager plus d'équipes

--- Index.php
+++ Index.php
@@ -12,20 +12,28 @@ $espagne = new Pays("Espagne");
 //Instancier des EQUIPES
 $ddnMbappe = new DateTime(("20-12-1998"));
 $ddnlionelmessi = new DateTime(("24-06-1987"));
-$FCBarca = new Equipe("FC Barcelone", 1899, $espagne);
+$fcBarca = new Equipe("FC Barcelone", 1899, $espagne);
 $realMadrid = new Equipe ("Real Madrid",1910, $espagne);
-$PSG = new Equipe("Paris Saint-Germain", 1970, $france);
-$RCStbg = new Equipe("Racing Club Strasbourg", 1906, $france);
+$psg = new Equipe("Paris Saint-Germain", 1970, $france);
+$rcStbg = new Equipe("Racing Club Strasbourg", 1906, $france);
+$manchesterUnited = new Equipe("Manchester United", 1878, $angleterre);
+$juv = new Equipe("Juventus Turin", 1897, $italie);
 //Instacnier des JOUEURS
 $kylianmbapon = new Joueur("Mbappé", "Kylian", $ddnMbappe, "Française");
 $lionelMessi = new Joueur("Messi", "Lionel", $ddnlionelmessi, "Argentine");
+
 //Instancier des SIGNATURES
-$signatureMbappePSG = new Signature("2017",$PSG, $kylianmbapon);
-$signatureMessiBarca = new Signature("2004",$FCBarca,$lionelMessi);
-$ignatureMessiPSG = new Signature("2021", $PSG, $lionelMessi );
-//AFFICHAGE
+$signatureMbappepsg = new Signature("2017",$psg, $kylianmbapon);
+$signatureMessiBarca = new Signature("2004",$fcBarca,$lionelMessi);
+$ignatureMessipsg = new Signature("2021", $psg, $lionelMessi );
+//AFFICHAGE Équipe par pays
 echo $france->afficherEquipes();
 echo $espagne->afficherEquipes();
-//$PSG->ajouterJoueur($kylianmbapon);
-echo $PSG->afficherJoueur();
+echo $angleterre->afficherEquipes();
+echo $italie->afficherEquipes();
+//Affichage des joueurs dans une équipe
+echo $psg->afficherJoueurs();
+echo $rcStbg->afficherJoueurs();
+echo $fcBarca->afficherJoueurs();
+echo $juv->afficherJoueurs();
 echo $lionelMessi->afficherSignatures();
