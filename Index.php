2842f9f523c5cb301ea615ad99e1252e6892f71a
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-03-29 14:22:30 +0200

    instancier tous les joueurs et toutes les équipes

--- Index.php
+++ Index.php
@@ -9,9 +9,12 @@ $allemagne = new Pays("Allemagne");
 $angleterre = new Pays("Angleterre");
 $italie = new Pays("Italie");
 $espagne = new Pays("Espagne");
-//Instancier des EQUIPES
+//Instancier des Dates de naissances
 $ddnMbappe = new DateTime(("20-12-1998"));
 $ddnlionelmessi = new DateTime(("24-06-1987"));
+$ddnNeymar = new DateTime(("05-02-1992"));
+$ddnCr7 = new DateTime(("05-02-1985"));
+//Instancier des EQUIPES 
 $fcBarca = new Equipe("FC Barcelone", 1899, $espagne);
 $realMadrid = new Equipe ("Real Madrid",1910, $espagne);
 $psg = new Equipe("Paris Saint-Germain", 1970, $france);
@@ -21,11 +24,17 @@ $juv = new Equipe("Juventus Turin", 1897, $italie);
 //Instacnier des JOUEURS
 $kylianmbapon = new Joueur("Mbappé", "Kylian", $ddnMbappe, "Française");
 $lionelMessi = new Joueur("Messi", "Lionel", $ddnlionelmessi, "Argentine");
-
+$neymarjunior = new Joueur("Neymar", "Junior", $ddnNeymar, "Brésilienne");
+$cr7 = new Joueur("Cristiano","Ronaldo", $ddnCr7, "Portugaise");
 //Instancier des SIGNATURES
-$signatureMbappepsg = new Signature("2017",$psg, $kylianmbapon);
-$signatureMessiBarca = new Signature("2004",$fcBarca,$lionelMessi);
-$ignatureMessipsg = new Signature("2021", $psg, $lionelMessi );
+$signatureMbappepsg = new Signature(2017,$psg, $kylianmbapon);
+$signatureMessiBarca = new Signature(2004,$fcBarca,$lionelMessi);
+$ignatureMessipsg = new Signature(2021, $psg, $lionelMessi );
+$signatureCr7Juv = new Signature(2018,$juv,$cr7);
+$signatureCr7Manchester = new Signature(2021, $manchesterUnited, $cr7);
+$signatureCr7Real = new Signature(2009,$realMadrid,$cr7);
+$signatureNeymarPSG = new Signature(2017, $psg,$neymarjunior);
+$signatureNeymarBarca = new Signature(2013, $fcBarca,$neymarjunior);
 //AFFICHAGE Équipe par pays
 echo $france->afficherEquipes();
 echo $espagne->afficherEquipes();
@@ -36,4 +45,8 @@ echo $psg->afficherJoueurs();
 echo $rcStbg->afficherJoueurs();
 echo $fcBarca->afficherJoueurs();
 echo $juv->afficherJoueurs();
+//Afficher dans quelles équipes un joueur a joué 
+echo $kylianmbapon->afficherSignatures();
 echo $lionelMessi->afficherSignatures();
+echo $cr7->afficherSignatures();
+echo $neymarjunior->afficherSignatures();
